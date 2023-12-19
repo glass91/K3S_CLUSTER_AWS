ecbd34602e435567ad2e80775d307100634d6034
ggg <ggg>
2023-12-19 17:47:02 -0500

    vpc

--- cluster_init/terraform/master_node_config/auto-scaling-group-master.tf
+++ cluster_init/terraform/master_node_config/auto-scaling-group-master.tf
@@ -7,7 +7,7 @@ resource "aws_autoscaling_group" "k3s_master_asg" {
   min_size            = 1
   max_size            = 1
   desired_capacity    = 1
-  vpc_zone_identifier = [data.aws_subnets.k3s_public_subnet.ids[1]]
+  vpc_zone_identifier = [data.aws_subnets.k3s_public_subnet.ids[0]]
   health_check_type         = "EC2"
   health_check_grace_period = 300
   force_delete              = true
