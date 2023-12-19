869670ca9951651efa32cf000eddddeeef21f002
ggg <ggg>
2023-12-19 15:51:59 -0500

    direction

--- create_cluster.groovy
+++ create_cluster.groovy
@@ -20,7 +20,7 @@ pipeline {
         stage('Terraform Plan - Main VPC') {
             steps {
                 sh '''
-                cd ./k3s_cluster_aws/cluster_init/terraform/main_vpc_config
+                cd ./cluster_init/terraform/main_vpc_config
                 terraform init -input=false
                 terraform plan -out=terraform.tfplan
                 '''
@@ -37,7 +37,7 @@ pipeline {
         stage('Terraform Apply - Main VPC') {
             steps {
                 sh '''
-                cd ./k3s_cluster_aws/cluster_init/terraform/main_vpc_config
+                cd ./cluster_init/terraform/main_vpc_config
                 terraform apply -input=false terraform.tfplan
                 '''
             }
@@ -45,7 +45,7 @@ pipeline {
         stage('Terraform Plan - Master Node') {
             steps {
                 sh '''
-                cd ./k3s_cluster_aws/cluster_init/terraform/master_node_config
+                cd ./cluster_init/terraform/master_node_config
                 terraform init -input=false
                 terraform plan -out=terraform.tfplan
                 '''
@@ -62,7 +62,7 @@ pipeline {
         stage('Terraform Apply - Master Node') {
             steps {
                 sh '''
-                cd ./k3s_cluster_aws/cluster_init/terraform/master_node_config
+                cd ./cluster_init/terraform/master_node_config
                 terraform apply -input=false terraform.tfplan
                 terraform output -json k3s_master_instance_private_ip | jq -r 'if type == "array" then .[] else . end' > ../../ansible/master_ip.txt
                 terraform output -json k3s_master_instance_public_ip | jq -r 'if type == "array" then .[] else . end' > ../../ansible/master_ip_public.txt
@@ -72,7 +72,7 @@ pipeline {
         stage('Terraform Plan - Worker Nodes') {
             steps {
                 sh '''
-                cd ./k3s_cluster_aws/cluster_init/terraform/worker_node_config
+                cd ./cluster_init/terraform/worker_node_config
                 terraform init -input=false
                 terraform plan -out=terraform.tfplan
                 '''
@@ -89,7 +89,7 @@ pipeline {
         stage('Terraform Apply - Worker Nodes') {
             steps {
                 sh '''
-                cd ./k3s_cluster_aws/cluster_init/terraform/worker_node_config
+                cd ./cluster_init/terraform/worker_node_config
                 terraform apply -input=false terraform.tfplan
                 sleep 0
                 terraform output -json k3s_workers_instance_private_ip | jq -r '.[]' > ../../ansible/worker_ip.txt
