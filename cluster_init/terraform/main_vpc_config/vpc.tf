ef30e2a71d9902440c9e78e5a3ae0b2ab6fbbc4c
ggg <ggg>
2023-12-20 22:45:24 -0500

    line 36

--- cluster_init/terraform/main_vpc_config/vpc.tf
+++ cluster_init/terraform/main_vpc_config/vpc.tf
@@ -33,7 +33,7 @@ resource "aws_subnet" "k3s_public_subnet" {
 resource "aws_subnet" "k3s_private_subnet" {
   vpc_id     = aws_vpc.k3s_vpc.id
   cidr_block = "10.0.2.0/24"
-
+  availability_zone = "us-east-1b"
   tags = {
     Name = "K3s_Private_Subnet"
   }
